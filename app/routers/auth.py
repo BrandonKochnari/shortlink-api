946870cbb7b5ed081f0309c9e73846aba0a41ea7
brandon <brandon.kochnari@gmail.com>
2026-05-29 00:33:10 -0400

    Add JWT authentication and current user endpoint (day 2.5)

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,5 +1,12 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
+from fastapi.security import OAuth2PasswordBearer
+from app.services.security import (
+    hash_password,
+    verify_password,
+    create_access_token,
+    decode_access_token
+)
 
 from app.database import get_db
 from app.models import User
@@ -13,6 +20,10 @@ from app.services.security import (
     hash_password,
     verify_password,
     create_access_token
+)
+
+oauth2_scheme = OAuth2PasswordBearer(
+    tokenUrl="/api/v1/auth/login"
 )
 
 router = APIRouter()
@@ -70,3 +81,31 @@ def login(user_data: UserLogin, db: Session = Depends(get_db)):
         "access_token": access_token,
         "token_type": "bearer"
     }
+
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+    payload = decode_access_token(token)
+    user_id = payload.get("sub")
+    if user_id is None:
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid token"
+        )
+    user = (
+        db.query(User)
+        .filter(User.id == int(user_id))
+        .first()
+    )
+    if not user:
+        raise HTTPException(
+            status_code=401,
+            detail="User not found"
+        )
+    return user
+
+@router.get(
+    "/me",
+    response_model = UserResponse
+)
+
+def get_me(current_user: User = Depends(get_current_user)):
+    return current_user
